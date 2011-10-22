b8ca604b65f8bbb0c42f482888dcaa851db3e664
Your Name <matthew.fong@gmail.com>
2011-10-22 00:19:09 -0400

    Fixed selector  issue

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,7 +11,7 @@ describe PagesController do
     
     it "should have the right title" do
       get'home'
-      response.should have_slector("title",
+      response.should have_selector("title",
         :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
@@ -24,7 +24,7 @@ describe PagesController do
     
     it "should have the right title" do
       get'contact'
-      response.should have_slector("title",
+      response.should have_selector("title",
         :content => "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
@@ -37,7 +37,7 @@ describe PagesController do
     
     it "should have the right title" do
       get'about'
-      response.should have_slector("title",
+      response.should have_selector("title",
         :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
